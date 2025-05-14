845db45fa3502f8ae15909ae7bf1ba1c682fb167
Dean Amar <Dean.Amar@ibm.com>
2025-05-14 21:57:04 +0300

    * separate tls option tests.

--- integration/test/tls_connection_test.go
+++ integration/test/tls_connection_test.go
@@ -11,7 +11,7 @@ import (
 	"github.ibm.com/decentralized-trust-research/scalable-committer/integration/runner"
 )
 
-func TestTLSConnection(t *testing.T) {
+func TestMutualTLSConnection(t *testing.T) {
 	t.Parallel()
 	gomega.RegisterTestingT(t)
 	c := runner.NewRuntime(t, &runner.Config{
@@ -22,6 +22,30 @@ func TestTLSConnection(t *testing.T) {
 		TLS: runner.TLSSettings{
 			UseTLS:    true,
 			MutualTLS: true,
+		},
+	})
+
+	c.Start(t, runner.FullTxPathWithLoadGen)
+
+	require.Eventually(t, func() bool {
+		count := c.CountStatus(t, protoblocktx.Status_COMMITTED)
+		t.Logf("count %d", count)
+		return count > 10_000
+	}, 90*time.Second, 500*time.Millisecond)
+	require.Zero(t, c.CountAlternateStatus(t, protoblocktx.Status_COMMITTED))
+}
+
+func TestOneSidedTLSConnection(t *testing.T) {
+	t.Parallel()
+	gomega.RegisterTestingT(t)
+	c := runner.NewRuntime(t, &runner.Config{
+		NumVerifiers: 2,
+		NumVCService: 2,
+		BlockTimeout: 2 * time.Second,
+		BlockSize:    500,
+		TLS: runner.TLSSettings{
+			UseTLS:    true,
+			MutualTLS: false,
 		},
 	})
 
