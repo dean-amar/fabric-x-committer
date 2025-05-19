484d21912da8545f2a7b9b110f862cb4db03e64a
Dean Amar <Dean.Amar@ibm.com>
2025-05-19 12:30:16 +0300

    * Fixed container_test

--- docker/test/container_test.go
+++ docker/test/container_test.go
@@ -163,7 +163,7 @@ func startCommitter(
 			"SC_SIDECAR_ORDERER_CONNECTION_ENDPOINTS=" + serviceEndpoint(ordererPort),
 			"SC_LOADGEN_ORDERER_CLIENT_ORDERER_CHANNEL_ID=" + channelName,
 			"SC_LOADGEN_ORDERER_CLIENT_ORDERER_CONNECTION_ENDPOINTS=" + serviceEndpoint(ordererPort),
-			"SC_LOADGEN_ORDERER_CLIENT_SIDECAR_ENDPOINT=" + serviceEndpoint(sidecarPort),
+			"SC_LOADGEN_ORDERER_CLIENT_SIDECAR_CLIENT_CONFIG_ENDPOINT=" + serviceEndpoint(sidecarPort),
 			"SC_QUERY_SERVER_ENDPOINT=:" + queryServicePort,
 		},
 		Tty: true,
