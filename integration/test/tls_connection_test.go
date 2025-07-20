9f7e95c9e94fa139caa9ec3e7d3431559e2413fb
Dean Amar <Dean.Amar@ibm.com>
2025-07-20 18:16:18 +0300

    * check commit
    Signed-off-by: Dean Amar <Dean.Amar@ibm.com>

--- integration/test/tls_connection_test.go
+++ integration/test/tls_connection_test.go
@@ -64,7 +64,8 @@ func TestOneSidedTLSConnection(t *testing.T) {
 
 func TestMutualTLSConnectionAndDatabaseTLS(t *testing.T) {
 	t.Parallel()
-	for _, dbType := range []string{dbtest.YugaDBType, dbtest.PostgresDBType} {
+	//, dbtest.PostgresDBType
+	for _, dbType := range []string{dbtest.YugaDBType} {
 		databaseType := dbType
 		t.Run(fmt.Sprintf("%s_tls", databaseType), func(t *testing.T) {
 			t.Parallel()
