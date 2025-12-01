9f025d0d0bde686bb14a7625636c8d12bdae5c3c
Dean Amar <Dean.Amar@ibm.com>
2025-12-01 20:11:22 +0200

    * tryout
    Signed-off-by: Dean Amar <Dean.Amar@ibm.com>

--- service/vc/dbtest/container.go
+++ service/vc/dbtest/container.go
@@ -34,7 +34,7 @@ const (
 	defaultDBDeploymentTemplateName = "sc_%s_unit_tests"
 
 	defaultHostIP  = "127.0.0.1"
-	defaultPortMap = "7000/tcp"
+	defaultPortMap = "9555/tcp"
 
 	// container's Memory and CPU management.
 	gb         = 1 << 30 // gb is the number of bytes needed to represent 1 GB.
